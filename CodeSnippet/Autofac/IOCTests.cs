ca3635c31678e9f69fc68398cf47d452e257c02b
WangPengLiang <15101587969@163.com>
2021-04-07 18:07:44 +0800

    add autofac

---
+++ CodeSnippet/Autofac/IOCTests.cs
@@ -0,0 +1,85 @@
+﻿namespace CodeSnippet.Autofac.IOCTests
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestCategory("IOC")]
+    [TestClass()]
+    public class IOCTests
+    {
+        /// <summary>构造函数注入</summary>
+        [TestMethod()]
+        public void IOC_Example_CtorInject()
+        {
+            Order order = new Order(new SqlDal());
+            order.Add();
+            Assert.IsTrue(true);
+        }
+
+        /// <summary>属性注入</summary>
+        [TestMethod()]
+        public void IOC_Example_PropertyInject()
+        {
+            Order2 order = new Order2
+            {
+                Dal = new SqlDal()
+            };
+            order.Add();
+            Assert.IsTrue(true);
+        }
+    }
+
+    class Order
+    {
+        private readonly IDal dal;
+
+        /// <summary>
+        /// 构造函数注入
+        /// </summary>
+        /// <param name="dal"></param>
+        public Order(IDal dal)
+        {
+            this.dal = dal;
+        }
+
+        public void Add()
+        {
+            dal.Add();
+        }
+    }
+
+    class Order2
+    {
+        /// <summary>
+        /// 属性注入
+        /// </summary>
+        public IDal Dal { get; set; }
+
+        public void Add()
+        {
+            Dal.Add();
+        }
+    }
+
+    interface IDal
+    {
+        void Add();
+    }
+
+    class SqlDal : IDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("使用SqlDal插入一条数据");
+        }
+    }
+
+    class MySqlDal : IDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("使用MySqlDal插入一条数据");
+        }
+    }
+}
---
+++ CodeSnippet/Autofac/TraditionTests.cs
@@ -0,0 +1,63 @@
+﻿namespace CodeSnippet.Autofac.TraditionTests
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestCategory("IOC")]
+    [TestClass()]
+    public class TraditionTests
+    {
+        [TestMethod()]
+        public void Tradition_SqlDal()
+        {
+            Order1 order = new Order1();
+            order.Add();
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void Tradition_MySqlDal()
+        {
+            Order2 order = new Order2();
+            order.Add();
+            Assert.IsTrue(true);
+        }
+    }
+
+    class Order1
+    {
+        readonly SqlDal sqlDal = new SqlDal();
+
+        public void Add()
+        {
+            sqlDal.Add();
+        }
+    }
+
+    class Order2
+    {
+        readonly MySqlDal mysqlDal = new MySqlDal();
+
+        public void Add()
+        {
+            mysqlDal.Add();
+        }
+    }
+
+    class SqlDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("使用SqlDal插入一条数据");
+        }
+    }
+
+    class MySqlDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("使用MySqlDal插入一条数据");
+        }
+    }
+}
