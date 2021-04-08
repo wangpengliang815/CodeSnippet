a7ad0454cd962afbb5fabfb09f6177212f890c6c
WangPengLiang <15101587969@163.com>
2021-04-08 18:11:06 +0800

    add 数据保护

---
+++ DataProtection/WebApp01/Controllers/EnvironmentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataProtectionTests.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnvironmentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetEnvironmentVariables()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ DataProtection/WebApp01/Startup.cs
@@ -0,0 +1,43 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DataProtectionTests
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services,
+            IWebHostEnvironment env)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
